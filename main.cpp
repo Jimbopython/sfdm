da4983b53b26eb68adc635dd75c19e14311c4b35
Jimbopython <le.stoffers@gmail.com>
2025-12-25 20:57:00 +0100

    implement an overall test

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ std::map<std::string, std::vector<std::string>> readDataMatrixFile(const std::st
     return result;
 }
 
-void dmtx(const cv::Mat& cvImage, const std::vector<std::string>& data ) {
+int dmtx(const cv::Mat& cvImage, const std::vector<std::string>& data ) {
     // Create libdmtx image
     DmtxImage* image = dmtxImageCreate(
         cvImage.data,
@@ -72,11 +72,12 @@ void dmtx(const cv::Mat& cvImage, const std::vector<std::string>& data ) {
     std::vector<std::string> tmpData;
 
     for (int foundCodeCount = 0; foundCodeCount < data.size(); ++foundCodeCount) {
-        CAPTURE(foundCodeCount, data.size());
 
         // Find the next Data Matrix symbol
         DmtxRegion* region = dmtxRegionFindNext(decoder, nullptr);
-        REQUIRE(region != nullptr);
+        if(region == nullptr) {
+            continue;
+        }
         // Decode the symbol
         DmtxMessage* message = dmtxDecodeMatrixRegion(
             decoder,
@@ -84,25 +85,28 @@ void dmtx(const cv::Mat& cvImage, const std::vector<std::string>& data ) {
             DmtxUndefined
         );
 
-        REQUIRE(message != nullptr);
+        if(message == nullptr) {
+            continue;
+        }
 
         auto it = std::ranges::find(data, reinterpret_cast<const char*>(message->output));
-        REQUIRE(it != data.end());
-        tmpData.emplace_back(*it);
+        if(it != data.end()) {
+            tmpData.emplace_back(*it);
+        }
 
         dmtxMessageDestroy(&message);
 
         dmtxRegionDestroy(&region);
     }
 
-    REQUIRE(containsAll(tmpData, data));
-
     // Cleanup
     dmtxDecodeDestroy(&decoder);
     dmtxImageDestroy(&image);
+
+    return tmpData.size();
 }
 
-void zxing(const cv::Mat& cvImage, const std::vector<std::string>& data) {
+int zxing(const cv::Mat& cvImage, const std::vector<std::string>& data) {
     ZXing::ImageView source{cvImage.data, cvImage.cols, cvImage.rows, ZXing::ImageFormat::Lum};
 
     ZXing::ReaderOptions options;
@@ -111,43 +115,75 @@ void zxing(const cv::Mat& cvImage, const std::vector<std::string>& data) {
     const auto results = ZXing::ReadBarcodes(source, options);
 
     std::vector<std::string> tmpData;
-    int foundCount = 0;
     for (const auto& result : results) {
-        ++foundCount;
-        CAPTURE(foundCount, data.size());
         auto it = std::ranges::find(data, result.text());
-        REQUIRE(it != data.end());
-        tmpData.emplace_back(*it);
+        if(it != data.end()) {
+            tmpData.emplace_back(*it);
+        }
     }
-    REQUIRE(tmpData.size() == data.size());
-    REQUIRE(containsAll(tmpData, data));
+    return tmpData.size();
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
+cv::Mat get_image(const std::filesystem::directory_entry &entry) {
+    cv::Mat image = cv::imread(entry.path().string(), cv::IMREAD_COLOR);
+
+    // Convert to 8-bit grayscale
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+
+    // Ensure it's 8-bit (usually already is)
+    gray.convertTo(gray, CV_8U);
+    return gray;
+}
+
+template <typename Callable>
+void test(const Callable& callable) {
     const auto data = readDataMatrixFile("_deps/images-src/annotations.txt");
 
-    for (const auto& entry : std::filesystem::directory_iterator("_deps/images-src")) {
-        if (entry.is_regular_file() && entry.path().extension() == ".jpg") {
-            cv::Mat image = cv::imread(entry.path().string(), cv::IMREAD_COLOR);
+    SECTION("Single") {
+        for (const auto& entry : std::filesystem::directory_iterator("_deps/images-src")) {
+            if (entry.is_regular_file() && entry.path().extension() == ".jpg") {
+                cv::Mat image = get_image(entry);
 
-            // Convert to 8-bit grayscale
-            cv::Mat gray;
-            cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
-
-            // Ensure it's 8-bit (usually already is)
-            gray.convertTo(gray, CV_8U);
-
-            const auto fileName = entry.path().stem().string();
-            const auto it = data.find(fileName);
-            if (it == data.end()) {
-                continue;
-            }
-            SECTION("LibDMTX - " + fileName) {
-                dmtx(gray, it->second);
-            }
-            SECTION("ZXing - " + fileName) {
-                zxing(gray, it->second);
+                const auto fileName = entry.path().stem().string();
+                const auto it = data.find(fileName);
+                if (it == data.end()) {
+                    continue;
+                }
+                const auto currentData = it->second;
+                SECTION(fileName) {
+                    const auto foundCount = callable(image, currentData);
+                    REQUIRE(foundCount == currentData.size());
+                }
             }
         }
     }
+    SECTION("Overall") {
+        int foundTotal = 0;
+        int totalCodes = 0;
+        for (const auto& entry : std::filesystem::directory_iterator("_deps/images-src")) {
+            if (entry.is_regular_file() && entry.path().extension() == ".jpg") {
+                cv::Mat image = get_image(entry);
+
+                const auto fileName = entry.path().stem().string();
+                const auto it = data.find(fileName);
+                if (it == data.end()) {
+                    continue;
+                }
+                const auto currentData = it->second;
+                const auto foundCount = callable(image, currentData);
+                foundTotal += foundCount;
+                totalCodes += currentData.size();
+            }
+        }
+        REQUIRE(foundTotal == totalCodes);
+    }
+}
+
+TEST_CASE( "LibDMTX" ) {
+    test(dmtx);
+}
+
+TEST_CASE( "ZXing" ) {
+    test(zxing);
 }
