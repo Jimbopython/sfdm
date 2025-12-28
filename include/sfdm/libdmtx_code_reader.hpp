7ca0c87cec4cb37783ba92f6c4ffeb9d75ffe752
Jimbopython <le.stoffers@gmail.com>
2025-12-28 22:38:06 +0100

    refactor libdmtx implementation by splitting detect and decode

--- include/sfdm/libdmtx_code_reader.hpp
+++ include/sfdm/libdmtx_code_reader.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include <sfdm/icode_reader.hpp>
 
+extern "C" {
+struct DmtxRegion_struct;
+struct DmtxDecode_struct;
+struct DmtxMessage_struct;
+}
+
 namespace sfdm {
     class LibdmtxCodeReader : public ICodeReader {
     public:
@@ -11,6 +17,13 @@ namespace sfdm {
         bool isTimeoutSupported() override;
 
     private:
+        [[nodiscard]] std::shared_ptr<DmtxRegion_struct>
+        detectNext(const std::shared_ptr<DmtxDecode_struct> &decoder) const;
+
+        [[nodiscard]] std::shared_ptr<DmtxMessage_struct>
+        decode(const std::shared_ptr<DmtxDecode_struct> &decoder,
+               const std::shared_ptr<DmtxRegion_struct> &region) const;
+
         uint32_t m_timeoutMSec{};
     };
 }
--- src/libdmtx_code_reader.cpp
+++ src/libdmtx_code_reader.cpp
@@ -7,10 +7,14 @@ namespace {
         explicit DecodeGuard(const cv::Mat &image) : m_image(dmtxImageCreate(
                                                                  image.data, image.cols, image.rows, DmtxPack8bppK),
                                                              [](DmtxImage *dmtxImage) {
-                                                                 dmtxImageDestroy(&dmtxImage);
+                                                                 if (dmtxImage) {
+                                                                     dmtxImageDestroy(&dmtxImage);
+                                                                 }
                                                              }), m_decoder(dmtxDecodeCreate(m_image.get(), 1),
                                                                            [](DmtxDecode *decoder) {
-                                                                               dmtxDecodeDestroy(&decoder);
+                                                                               if (decoder) {
+                                                                                   dmtxDecodeDestroy(&decoder);
+                                                                               }
                                                                            }) {
             if (!m_image) {
                 throw std::runtime_error("Could not create image!");
@@ -29,46 +33,51 @@ namespace {
         std::shared_ptr<DmtxImage> m_image;
         std::shared_ptr<DmtxDecode> m_decoder;
     };
-
-    template<typename T, auto Callable>
-    class GenericGuard {
-    public:
-        explicit GenericGuard(T *guardable) : m_guardable(guardable,
-                                                          [](T *dmtxGuardable) {
-                                                              Callable(&dmtxGuardable);
-                                                          }) {
-        }
-
-        std::shared_ptr<T> getGuardable() {
-            return m_guardable;
-        }
-
-    private:
-        std::shared_ptr<T> m_guardable;
-    };
 }
 
 namespace sfdm {
+    std::shared_ptr<DmtxRegion> LibdmtxCodeReader::detectNext(
+        const std::shared_ptr<DmtxDecode> &decoder) const {
+        DmtxTime timeout = dmtxTimeNow();
+        timeout = dmtxTimeAdd(timeout, m_timeoutMSec);
+        return {
+            dmtxRegionFindNext(decoder.get(), m_timeoutMSec ? &timeout : nullptr),
+            [](DmtxRegion *region) {
+                if (region) {
+                    dmtxRegionDestroy(&region);
+                }
+            }
+        };
+    }
+
+    [[nodiscard]] std::shared_ptr<DmtxMessage_struct>
+    LibdmtxCodeReader::decode(const std::shared_ptr<DmtxDecode_struct> &decoder,
+                              const std::shared_ptr<DmtxRegion_struct> &region) const {
+        return {
+            dmtxDecodeMatrixRegion(decoder.get(), region.get(), DmtxTrue),
+            [](auto *message) {
+                if (message) {
+                    dmtxMessageDestroy(&message);
+                }
+            }
+        };
+    }
+
     std::vector<DecodeResult> LibdmtxCodeReader::decode(const cv::Mat &image) const {
         DecodeGuard decodeGuard(image);
 
         std::vector<DecodeResult> results;
 
         for (int i = 0; i < 255; ++i) {
-            DmtxTime timeout = dmtxTimeNow();
-            timeout = dmtxTimeAdd(timeout, m_timeoutMSec);
-            const auto region = dmtxRegionFindNext(decodeGuard.getDecoder().get(), m_timeoutMSec ? &timeout : nullptr);
+            const auto region = detectNext(decodeGuard.getDecoder());
             if (!region) {
                 break;
             }
-            GenericGuard<DmtxRegion, dmtxRegionDestroy> regionGuard(region);
 
-            DmtxMessage *message =
-                    dmtxDecodeMatrixRegion(decodeGuard.getDecoder().get(), region, DmtxTrue);
+            const auto message = decode(decodeGuard.getDecoder(), region);
             if (!message) {
                 break;
             }
-            GenericGuard<DmtxMessage, dmtxMessageDestroy> messageGuard(message);
 
             results.emplace_back(DecodeResult{reinterpret_cast<const char *>(message->output)});
         }
