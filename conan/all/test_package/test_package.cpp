6192f8f454d9ab16d11a5fe41735fe95f91aa659
Jimbopython <le.stoffers@gmail.com>
2025-12-29 21:10:37 +0100

    add conan recipe prototype

---
+++ conan/all/test_package/test_package.cpp
@@ -0,0 +1,10 @@
+#include <sfdm/libdmtx_code_reader.hpp>
+#include <sfdm/zxing_code_reader.hpp>
+
+
+int main(void) {
+    sfdm::LibdmtxCodeReader dmtxCodeReader{};
+    sfdm::ZXingCodeReader zxingCodeReader{};
+
+    return 0;
+}
